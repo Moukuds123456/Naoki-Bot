ce49d0c8e1b4c9a7a82d7706f825a3caab9dcb3e
Moukuds123456 <154756570+Moukuds123456@users.noreply.github.com>
2023-12-24 19:23:27 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 module.exports = {
     app: {
         px: '+',
-        token: '',
-        owners: [""],
-        funny: '',
+        token: 'MTE4ODIxNTAxMTI0MDMyOTMyNw.GTXFYg.udFsOCZzMXkTXSSDuIPHe4np9c3Hz5IkcM2AFY',
+        owners: ["1139311421235146864"],
+        funny: '1139311421235146864',
         color: '#2f3136',
         footer: 'Protect',
         maxserver: '100',
